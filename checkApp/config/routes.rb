5c34210653c17976a0b503413939b7f3188257b2
Jorge Vargas <eduardo.borisq@gmail.com>
2022-09-06 16:43:02 -0500

    Add devise gem
    We add the devise gem for users authentication
    
    Co-authored-by: CZamora5 <czamora5@ucol.mx>
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>
    Co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>

--- checkApp/config/routes.rb
+++ checkApp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :admin do
       resources :attendances
       resources :employees
@@ -6,9 +7,9 @@ Rails.application.routes.draw do
 
       root to: "attendances#index"
     end
-  resources :attendances
-  resources :employees
-  resources :stores
+  # resources :attendances
+  # resources :employees
+  # resources :stores
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
