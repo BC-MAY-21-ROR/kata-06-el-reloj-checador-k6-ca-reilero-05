3e29e5c42dc541d795d7441e2b3f913da2d97fb3
elioflo <eliofloreschoque2357@gmail.com>
2022-09-07 20:15:13 -0300

    Add validations for Attendaces, Employees, and Stores. Validate unique fields. Validate patterns.
    co-authored-by: CZamora5 <czamora5@ucol.mx>
    co-authored-by: jorgeboris <eduardo.borisq@gmail.com>
    co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>

--- checkApp/app/models/attendance.rb
+++ checkApp/app/models/attendance.rb
@@ -1,3 +1,5 @@
 class Attendance < ApplicationRecord
+  validates :action, :check_datetime, presence: true
+  validates :action, inclusion: { in: %w(checkin checkout) }
   belongs_to :employee
 end
--- checkApp/app/models/store.rb
+++ checkApp/app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
     has_many :employees
+    validates_associated :employees
+
+    validates :name, :address, presence: true
 end
