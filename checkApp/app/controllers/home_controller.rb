bca0a1c78c5a3878dcfc5d019beca3f27c9a4821
EduardoBriceno89 <eduardbrice@outlook.com>
2022-09-09 18:26:20 -0500

    Add controller method for private number
    co-authored-by: CZamora5 <czamora5@ucol.mx>
    co-authored-by: elioflo <eliofloreschoque2357@gmail.com>
    co-authored-by: jorgeboris <eduardo.borisq@gmail.com>

--- checkApp/app/controllers/home_controller.rb
+++ checkApp/app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def make_attendance
+    employee = Employee.find_by(secret_number: params[:secret_number])
+    puts employee
+  end
 end
