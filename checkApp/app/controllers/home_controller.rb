63246f64a2df1d5bb973eaf28cd0535a00eb564b
elioflo <eliofloreschoque2357@gmail.com>
2022-09-09 21:07:46 -0300

    Add homepage backend functionality
    co-authored-by: CZamora5 <czamora5@ucol.mx>
    co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>
    co-authored-by: jorgeboris <eduardo.borisq@gmail.com>

--- checkApp/app/controllers/home_controller.rb
+++ checkApp/app/controllers/home_controller.rb
@@ -4,6 +4,20 @@ class HomeController < ApplicationController
 
   def make_attendance
     employee = Employee.find_by(secret_number: params[:secret_number])
-    puts employee
+    if employee
+      if employee.presence
+        employee.attendances.create(check_datetime: params[:data], action: 'checkout')
+      else
+        employee.attendances.create(check_datetime: params[:data], action: 'checkin')
+      end
+      employee.presence = !employee.presence
+      employee.update
+      format.html { redirect_to new_user_session_path, notice: "Attendance was successfully created." }
+    else
+      # No esta
+      # Muestro un mensaje de error, codigo invalido
+      format.html { redirect_to home_path, notice: "Invalid code." }
+      format.json { render :index}
+    end
   end
 end
