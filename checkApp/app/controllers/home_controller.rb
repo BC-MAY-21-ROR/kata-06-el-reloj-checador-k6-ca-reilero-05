33fd927f8d4045962a39927cf95fedb5e8d57939
Carlos Zamora <czamora5@ucol.mx>
2022-09-13 17:51:03 -0500

    Add validation to check if an user is active before registering the attendance
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>
    Co-authored-by: jorgeboris <eduardo.borisq@gmail.com>
    Co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>

--- checkApp/app/controllers/home_controller.rb
+++ checkApp/app/controllers/home_controller.rb
@@ -4,12 +4,14 @@ class HomeController < ApplicationController
 
   def check
     employee = Employee.find_by(secret_number: params[:secret_number])
-    if employee
+    if employee && employee.status
       employee.attendances.create(check_datetime: params[:data], action: employee.presence ? 'checkout' : 'checkin')
       employee.update(presence: !employee.presence)
-      redirect_to '/', flash: { msg: "You have successfully checked." }
+      redirect_to '/', flash: { msg: "You have successfully checked.", class: 'success' }
+    elsif employee
+      redirect_to '/', flash: { msg: "This secret number belongs to an inactive employee." , class: 'error'}
     else
-      redirect_to '/', flash: { msg: "Invalid secret number." }
+      redirect_to '/', flash: { msg: "Invalid secret number.", class: 'error' }
     end
   end
 end
