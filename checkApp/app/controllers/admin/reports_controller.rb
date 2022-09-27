71436480cdf0276fcf217833aae426f99d927adf
Jorge Vargas <eduardo.borisq@gmail.com>
2022-09-27 18:46:41 -0500

    Modifying reports
    We replace the report attendance by day and add the report absence by month
    
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>
    Co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>

--- checkApp/app/controllers/admin/reports_controller.rb
+++ checkApp/app/controllers/admin/reports_controller.rb
@@ -1,8 +1,13 @@
 module Admin
-    class ReportsController < Admin::ApplicationController
-      def index
-        @attendances = Attendance.where("action = 'checkin'")
-        @employees = Employee.all
-      end
+  class ReportsController < Admin::ApplicationController
+    def index
+      @attendances = Attendance.where("action = 'checkin'").count
+      @date = Date.today
+
+      @absence = Attendance.where("action = 'checkout'").size
+      @time = Time.zone.now
+      @days = @time.end_of_month.day
+      @result_absence = @days - @absence
     end
+  end
 end
